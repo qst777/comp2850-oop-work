2009c0701c6075267e6713405302fdbe50f78876
Quintin Thornton <quintin.thornton@nhs.net>
2025-10-06 09:55:11 +0100

    task 3.1.1

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,11 @@
 // Task 3.1: command line arguments
+import kotlin.system.exitProcess
+
+fun main(arg: Array<String>) {
+  if (arg.size != 2) {
+    println("Error: filename required as sole argument")
+    exitProcess(1)
+  }
+  
+  println("${arg[0]} ${arg[1]}")
+}
