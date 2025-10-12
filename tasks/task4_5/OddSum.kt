fe0b628738a8288ca6ebf83156f0b000be6f4e70
Quintin Thornton <quintin.thornton@nhs.net>
2025-10-12 16:48:33 +0100

    task 4.5

--- tasks/task4_5/OddSum.kt
+++ tasks/task4_5/OddSum.kt
@@ -1,1 +1,15 @@
 // Task 4.5: summing odd integers with a for loop
+
+fun main() {
+  val userNumber: Int
+
+  println("Pick a high number:")
+  userNumber = readln().toInt()
+
+  var total = 0
+  for (n in 1..userNumber step 2){
+    total += n
+  }
+
+  println("The total is $total")
+}
