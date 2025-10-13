d986f0e50b45ea73a0d67fb1a1d8cb6fed60dd96
Quintin Thornton <quintin.thornton@nhs.net>
2025-10-13 10:29:48 +0100

    task 5.1.1

--- tasks/task5_1_1/Anagrams.kt
+++ tasks/task5_1_1/Anagrams.kt
@@ -1,1 +1,19 @@
 // Task 5.1.1: anagram checking using a function
+fun main() {
+
+  println("please write down two words that might be anangrams of each other")
+
+  val word1: String = readln()
+  val word2: String = readln()
+
+  println(anagrams(word1, word2))
+}
+
+fun anagrams(first: String, second: String): Boolean {
+    if (first.length != second.length) {
+        return false
+    }
+    val firstChars = first.lowercase().toList().sorted()
+    val secondChars = second.lowercase().toList().sorted()
+    return firstChars == secondChars
+}
